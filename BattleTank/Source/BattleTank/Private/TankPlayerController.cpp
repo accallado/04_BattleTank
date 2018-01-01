0335c93d149d102645abb71a5e7586ef670f8b7a
Antonio Callado <accallado@gmail.com>
2018-01-01 14:28:49 -0600

    Got viewport size and calculated aiming dot screen position in pixels.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -45,7 +45,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	if (GetSightRayHitLocation(HitLocation))  // Has "side-effect": is going to linetrace
 	{
 
-		UE_LOG(LogTemp, Warning, TEXT("Hit location: %s"), *HitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
 		// TODO Tell controlled tank to aim at this point
 
 	}
@@ -58,5 +58,13 @@ void ATankPlayerController::AimTowardsCrosshair()
 bool ATankPlayerController::GetSightRayHitLocation(FVector & OutHitLocation) const
 {
 	OutHitLocation = FVector(1.0);
+	// Find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ScreenLocation = FVector2D((float)ViewportSizeX*CrossHairXLocation, (float)ViewportSizeY*CrossHairYLocation);
+	UE_LOG(LogTemp, Warning, TEXT("Aim screen location: %s"), *ScreenLocation.ToString());
+
+	// "De-project" the screen position of the crosshair to a world direction
+	// Linetrace along that look direction, and see what we hit (up to max range)
 	return true;
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,16 +14,21 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	ATank* GetControlledTank() const;
 	
 	void BeginPlay() override;
 
 private:
+	ATank* GetControlledTank() const;
+
+	UPROPERTY(EditAnywhere)
+		float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+		float CrossHairYLocation = 0.33333f;
 
 	// Start the tank moving the barrel so that a shot would it
 	// where the crosshair intersects the world
